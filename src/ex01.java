84619ebcce4fa5930d8d259ac7373bcb750a0ce6
Thiago Stteffen <thiagokalu@gmail.com>
2021-06-12 15:43:36 -0300

    Subindo o ex01

---
+++ src/ex01.java
@@ -0,0 +1,14 @@
+
+public class ex01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero=0;
+		while(numero<=1000) {
+			System.out.println(numero);
+			numero = numero +1;
+		}
+
+	}
+
+}
